9babe5e9c7868c48e4a0acb8efe4910d3418620f
BaslaSiripatana <79196014+BaslaSiripatana@users.noreply.github.com>
2021-03-04 22:01:11 +0700

    Update ssim_api.py

--- ssim_api.py
+++ ssim_api.py
@@ -9,13 +9,36 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 def load_images_from_folder(folder):
-    filename = os.listdir(folder)
+    # filename = os.listdir(folder)
+    filename = []
+    for img in os.listdir(folder):
+        if img.endswith("jpg"):
+            filename.append(img)
+
     return filename
 
 def img_comparison(input1, input2):
 
     imageA = cv2.imread(input1)
+    dimensions = imageA.shape
+
+    if dimensions[0]>dimensions[1]:
+        imageA = cv2.rotate(imageA, cv2.cv2.ROTATE_90_CLOCKWISE)
+        dimensions = imageA.shape
+
+    while dimensions[1]>1000 or dimensions[0]>1000: 
+        scale_percent = 25 # percent of original size
+        width = int(dimensions[1] * scale_percent / 100)
+        height = int(dimensions[0] * scale_percent / 100)
+        dim = (width, height)
+        imageA = cv2.resize(imageA, dim, interpolation = cv2.INTER_AREA)
+        dimensions = imageA.shape
+
     imageA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+    
+    dim = (dimensions[1],dimensions[0])
+
+    # imageA = cv2.resize(imageA, dim, interpolation = cv2.INTER_AREA)
 
     images = load_images_from_folder(input2)
 
@@ -23,6 +46,10 @@ def img_comparison(input1, input2):
     for image in images:
         img = cv2.imread(input2 + '\\' + image)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        dimensions = img.shape
+        if dimensions[0]>dimensions[1]:
+            img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
+        img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
         imageB.append(img)
 
     percent_ssim = []
@@ -58,4 +85,4 @@ def api_img_similarity():
     else:
         return jsonify(img_comparison(input1, input2))
 
-app.run()
+app.run(host='0.0.0.0')
